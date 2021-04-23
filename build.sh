a4b2454890739659e9b906dc5a7fddeec8cfcb42
Sander Niemeijer <sander.niemeijer@stcorp.nl>
2021-04-23 15:08:26 +0200

    Update VTK patch (we now only need to patch the path for the location of libXext)

--- build.sh
+++ build.sh
@@ -1,13 +1,9 @@
 export CMAKE_LIBRARY_PATH=$PREFIX/lib
 
 if test $BUILD == "x86_64-conda_cos6-linux-gnu" ; then
-  # Add missing dependency on libuuid.so
-  sed -i -e "s:\(/tmp/build/[^\/]*/vtk_[^\/]*/_build_env/x86_64-conda_cos6-linux-gnu/sysroot/usr/lib64/lib\)SM.so:\1uuid.so;\1SM.so:" "$PREFIX/lib/cmake/vtk-8.2/VTKTargets.cmake"
   # Patch the invalid paths to X libraries in VTKTargets.cmake
-  sed -i -e "s:/tmp/build/[^\/]*/vtk_[^\/]*/_build_env:$PREFIX:g" "$PREFIX/lib/cmake/vtk-8.2/VTKTargets.cmake"
-  cmake -DCMAKE_INSTALL_PREFIX="$PREFIX"
-else
-  cmake -DCMAKE_INSTALL_PREFIX="$PREFIX"
+  sed -i -e "s:/usr/lib64:$PREFIX/lib:g" "$PREFIX/lib/cmake/vtk-8.2/VTKTargets.cmake"
 fi
+cmake -DCMAKE_INSTALL_PREFIX="$PREFIX"
 make VERBOSE=1
 make install
