9256f5f0d12e6c81f048b0c039b50c2428db3aad
Sander Niemeijer <sander.niemeijer@stcorp.nl>
2021-04-23 15:31:31 +0200

    Use proper path and package for libXext.so

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@ export CMAKE_LIBRARY_PATH=$PREFIX/lib
 
 if test $BUILD == "x86_64-conda_cos6-linux-gnu" ; then
   # Patch the invalid paths to X libraries in VTKTargets.cmake
-  sed -i -e "s:/usr/lib64:$PREFIX/lib:g" "$PREFIX/lib/cmake/vtk-8.2/VTKTargets.cmake"
+  sed -i -e "s:/usr/lib64/libXext.so:$PREFIX/x86_64-conda-linux-gnu/sysroot/usr/lib64/libXext.so:g" "$PREFIX/lib/cmake/vtk-8.2/VTKTargets.cmake"
 fi
 cmake -DCMAKE_INSTALL_PREFIX="$PREFIX"
 make VERBOSE=1
